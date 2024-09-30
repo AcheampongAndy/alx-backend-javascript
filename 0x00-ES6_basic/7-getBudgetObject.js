57c0406ccbf90d656331104e647171b1e2d15f41
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-09-30 16:19:57 +0000

    shorthand

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,5 @@
-export default function getBudgetObject(income, gpa, capita) {
-	const budget = {income, gpa, capita};
+export default function getBudgetObject(income, gdp, capita) {
+	const budget = {income, gdp, capita};
 
 	return budget;
 }
