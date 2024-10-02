a3c23c7da34ce8305e6ea26b66f01ca910c0179e
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-02 15:06:48 +0000

    final

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,30 @@
-import { signUpUser } from './4-user-promise'
-import { uploadPhoto } from './5-photo-reject'
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-	const user = signUpUser(firstName, lastName)
-	.then((value) => ({
-		status: 'fulfilled',
-		value,
-	}));
-	
-	const photo = uploadPhoto(fileName)
-	.catch((error) => ({
-		status: 'rejected',
-		value: error.toString(),
-	}));
+    const user = signUpUser(firstName, lastName)
+        .then((value) => ({
+            status: 'fulfilled',
+            value,
+        }))
+        .catch((error) => ({
+            status: 'rejected',
+            value: error.toString(),
+        }));
 
-	const result = await Promise.all([ user, photo ]);
-	return result;
+    const photo = uploadPhoto(fileName)
+        .then((value) => ({
+            status: 'fulfilled',
+            value,
+        }))
+        .catch((error) => ({
+            status: 'rejected',
+            value: error.toString(),
+        }));
+
+    // Use Promise.allSettled to wait for both promises to settle
+    const result = await Promise.allSettled([user, photo]);
+
+    // Return the result array
+    return result;
 }
