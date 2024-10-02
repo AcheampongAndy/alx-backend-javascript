e55e55135fae5de789a28c350865b6778e0948e6
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-02 12:00:31 +0000

    done

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,12 @@
 export default function getFullResponseFromAPI(success) {
 	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (success) {
-				resolve({status: 200, body: 'success'});
-			} else {
-				reject(new Error('The fake API is not working currently'));
-			}
-		}, 1000);
+		if (success) {
+			resolve({
+				status: 200,
+				body: 'Success',
+			});
+		} else {
+			reject(new Error('The fake API is not working currently'));
+		}
 	});
 }
