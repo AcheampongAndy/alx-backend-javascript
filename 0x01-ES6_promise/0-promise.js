3c778c1998c6501642c020b5749b1283ff4c233d
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-02 10:25:59 +0000

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,13 @@
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		const success = true;
+    return new Promise((resolve, reject) => {
+        const success = true;
 
-		setTimeout(() => {
-			if (success) {
-				resolve('Success: Response from API received');
-			} else {
-				reject('Error: Failed to received response from API');
-			}
-		}, 1000);
-	});
+        setTimeout(() => {
+            if (success) {
+                resolve('Success: Response from API received');
+            } else {
+                reject('Error: Failed to received response from API');
+            }
+        }, 1000);
+    });
 }
