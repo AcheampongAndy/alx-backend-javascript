1b71afae03ef969bcbc56bc1643877d7bba9b3c6
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-02 15:00:52 +0000

    final

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import { signUpUser } from './4-user-promise'
+import { uploadPhoto } from './5-photo-reject'
+
+export default handleProfileSignup(firstName, lastName, fileName) {
+	const user = signUpUser(firstName, lastName)
+	.then((value) => ({
+		status: 'fulfilled',
+		value,
+	}));
+	
+	const photo = uploadPhoto(fileName)
+	.catch((error) => ({
+		status: 'rejected',
+		value: error.toString(),
+	}));
+
+	const result = await Promise.all([ user, photo ]);
+	return results;
+}
