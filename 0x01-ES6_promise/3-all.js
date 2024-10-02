faac454187643875dd5540e5f322a0e93e271d60
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-02 13:45:05 +0000

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
-// Importing the functions from utils.js
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhot, createUser } from './utils'
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadResponse, userResponse]) => {
-      console.log(uploadResponse.body, userResponse.firstName, userResponse.lastName);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+export default function handleProfileSignup() {
+	return Promise.all([ uploadPhoto(), createUser() ])
+	.then(([photo, user]) => {
+		console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+	})
+	.catch(() => {
+		console.log('Signup system offline');
+	});
 }
