4895528cbcbc9f24bbddccf3f50d4b827b7bd5b0
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-04 10:39:23 +0000

    course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export class HolbertonCourse {
+	constructor(name, length, students) {
+		if ((typeof name) != 'string') {
+			throw new TypeError('Name must be a string');
+		}
+		if ((typeof length) != 'number') {
+			throw new TypeError('Length must be a number');
+		}
+		if (!Array.isArray(students)) {
+			throw new TypeError('Must be an array');
+		}
+		this.name = name;
+		this.length = length;
+		this.students = students;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(newName) {
+		if ((typeof newName) != 'string') {
+			throw new TypeError('Name must be a string');
+		}
+		this._name = newName;
+	}
+
+	get length() {
+		return this._length;
+	}
+
+	set length(newLength) {
+		if ((typeof newLength) != 'number') {
+			throw new TypeError('Length must be a number');
+		}
+		this._length = Number(newLength);
+	}
+
+	get students() {
+		return this._students;
+	}
+
+	set students(newStudents) {
+		if (!Array.isArray(newStudents)) {
+			throw new TypeError('Must be array');
+		}
+		this._students = newStudents;
+	}
+}
