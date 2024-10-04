e38d94677ef330b42f4f7fcdfcf7370ab7e81c24
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-10-04 20:10:49 +0000

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,11 +1,13 @@
-import Currency from '3-currency.js';
+iiii// Pricing.js
+import Currency from './3-currency.js';
 
 export default class Pricing {
 	constructor(amount, currency) {
-		this._amount = amount;
-		this._currency = currency;
+		this._amount = amount; // Store the amount
+		this._currency = currency; // Store the currency (instance of Currency class)
 	}
 
+	// Getter and setter for amount
 	get amount() {
 		return this._amount;
 	}
@@ -14,6 +16,7 @@ export default class Pricing {
 		this._amount = newAmount;
 	}
 
+	// Getter and setter for currency (expects a Currency object)
 	get currency() {
 		return this._currency;
 	}
@@ -22,10 +25,12 @@ export default class Pricing {
 		this._currency = newCurrency;
 	}
 
+	// Method to display the full price as 'amount currency_name (currency_code)'
 	displayFullPrice() {
 		return `${this._amount} ${this._currency.name} (${this._currency.code})`;
 	}
 
+	// Static method to convert price based on a conversion rate
 	static convertPrice(amount, conversionRate) {
 		return amount * conversionRate;
 	}
